fd6d450042613dd32e442c636af09aa99696864a
Nahku Saidy <nsaidy@t-online.de>
2024-04-25 15:18:30 +0100

    feat: answer which is largest

--- src/main/java/com/oocode/Answerer.java
+++ src/main/java/com/oocode/Answerer.java
@@ -1,12 +1,29 @@
 package com.oocode;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Answerer {
     public String answerFor(String question) {
+        question = question.replace("?", "");
+
+        if (question.startsWith("Which of the following numbers is the largest:")){
+            return Integer.toString(getMax(question));
+        }
+
         switch (question){
-            case "What is your name?":
+            case "What is your name":
                 return "ns";
             default:
                 return "Someone";
         }
+    }
+
+    private int getMax(String s) {
+        String numbers = s.split(":")[1];
+        int[] singleNumbers = Arrays.stream(numbers.split(",")).mapToInt(Integer::parseInt).toArray();
+        return Arrays.stream(singleNumbers)
+                .max()
+                .getAsInt();
     }
 }
